d3f0533767f2b2d2c1fd902b562bd8a42fac7114
soanakolokimoana <48263792+soanakolokimoana@users.noreply.github.com>
2019-12-06 07:41:38 +1100

    feat(product) : update home-page

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -13,7 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ProfileController extends AbstractController
 {
     /**
-     * @Route("/", name="profile", methods={"GET"})
+     * @Route("/mon-profil", name="profile", methods={"GET,"POST"})
      */
     public function show()
     {
